6f038ab623f3eea39d8e569bf58a00bd5a6b1a6f
Vinay Setty <vinay@posteo.net>
2019-10-16 09:41:31 +0200

    added comparator code

---
+++ src/main/java/comparator/Song.java
@@ -0,0 +1,64 @@
+package comparator;
+
+public class Song implements Comparable <Song>
+{
+    String title;
+    String artist;
+    int rating;
+    String bpm;
+    
+    
+    public Song(String t, String a, int r, String b) {
+        title = t;
+        artist = a;
+        rating = r;
+        bpm = b;
+    }
+    
+    public Song(String t, String a, String r, String b) {
+        title = t;
+        artist = a;
+        rating = Integer.parseInt(r);
+        bpm = b;
+    }
+    
+    public boolean equals(Object aSong) {
+        Song s = (Song) aSong;
+        return getTitle().equals(s.getTitle());
+    }
+    
+    public int hashCode() {
+        return title.hashCode();
+    }
+    
+    public int compareTo(Song s)
+    {
+        return title.compareTo(s.getTitle());
+    }
+
+    public String getArtist()
+    {
+        return artist;
+    }
+
+    public String getBpm()
+    {
+        return bpm;
+    }
+
+    public int getRating()
+    {
+        return rating;
+    }
+
+    public String getTitle()
+    {
+        return title;
+    }
+    
+    public String toString() {
+        return title;
+    }
+
+}
+
